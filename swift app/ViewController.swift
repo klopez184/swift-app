20e3d725c979ed11872081d51376f30c8f7fe295
Kevin Lopez Blazquez <klopez184@outlook.com>
2017-06-19 10:41:28 -0400

    Final with boolean(at least what the tutorial said

--- swift app/ViewController.swift
+++ swift app/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var theLabel: UILabel!
     
     @IBOutlet weak var text1: UITextField!
@@ -20,20 +20,34 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: AnyObject) {
         
-        theLabel.text = "Answer is... \(Double(text1.text!)! + Double(text2.text!)!)"
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let addition = true
+        
+        if addition
+        {
+            
+            theLabel.text = "Answer is... \(Double(text1.text!)! + Double(text2.text!)!)"
+        }
+        else
+        {
+            theLabel.text = "Answer is... \(Double(text1.text!)! - Double(text2.text!)!)"
+        }
         
     }
+}
+/*
+override func viewDidLoad()
+{
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+override func didReceiveMemoryWarning()
+{
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+}
 
 
 }
-
+*/
